c345426ebbdfd40a546af504b37ef758b69b5080
naman708 <83646450+naman708@users.noreply.github.com>
2023-09-24 18:48:10 +0530

    Request / Response from server

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const http = require('http');
 
-const http=require('http');
-const server=http.createServer((req,res)=>{
-    res.end('naman pawar');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Define custom responses based on the URL
+  if (url === '/home') {
+    res.end('Welcome home');
+  } else if (url === '/about') {
+    res.end('Welcome to About Us page');
+  } else if (url === '/node') {
+    res.end('Welcome to my Node.js project');
+  } else {
+    // Handle other URLs with a 404 response
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+const port = 4000; 
+server.listen(port, () => {
 
 });
-server.listen(4000);
