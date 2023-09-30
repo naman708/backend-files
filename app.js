7644cf3c175f58c81fae6053f14bc486b6048daa
naman708 <83646450+naman708@users.noreply.github.com>
2023-09-30 15:32:56 +0530

    adding routes and filtering them

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 
 const express = require('express');
-const app = express();
 const bodyParser=require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><label for="productTitle">Product Name</label><input type="text" name="productTitle"><label for="productSize">Product size</label><input type="text" name="productSize"><button type="submit">Add Product</button></form>');
-})
-app.post('/product',(req,res,next)=>{
-   console.log(req.body);
-   res.redirect('/');
-})
+const app = express();
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1>hi there</h1>');
-})
+
+const adminRoutes=require('./routes/admin');
+const homeRoutes=require('./routes/home');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use('/shop',homeRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 
 app.listen(3000);
